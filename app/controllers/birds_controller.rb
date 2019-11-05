ca3bd9dd4d512f43f97f586a791ed302243cde17
David Acosta <davidacosta25081@yahoo.com>
2019-11-05 07:15:08 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,7 +6,7 @@ class BirdsController < ApplicationController
 
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: bird, except: [:created_at, :updated_at]
+    render json: {id: bird.id, name: bird.name, species: bird.species } 
   end
 
 
